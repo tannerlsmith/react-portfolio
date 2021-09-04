a0c67065183f83893e93234e55fb64a6d51909ce
tannerlsmith <tanner.smith@uvm.edu>
2021-09-03 20:44:34 -0400

    created navbar and connected component to app.js, including style sheet

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+
+
+function Navbar() {
+    // return(JSX)
+    return (
+        <header className="flex-row px-1 navbar">
+            <h2>
+                Tanner Smith
+            </h2>
+            <nav>
+                <ul>
+                    <li>
+                        <a data-testid="about" href="">
+                            About me
+                        </a>
+                    </li>
+                    <li>
+                        <a data-testid="projects" href="">
+                            Projects
+                        </a>
+                    </li>
+                    <li>
+                        <a data-testid="contact" href="">
+                            Contact
+                        </a>
+                    </li>
+                    <li>
+                        <a data-testid="resume" href="">
+                        Resume
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        
+    )
+}
+
+export default Navbar
