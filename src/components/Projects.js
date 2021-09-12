b393b2cbe8a944a68860d099a9e1b8a93818044c
tannerlsmith <tanner.smith@uvm.edu>
2021-09-11 20:57:44 -0400

    added project images and updated the styles for this component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,14 +9,15 @@ function Projects() {
             <Row>
                 <Col>
                     <section class="box">
-                        <a href="" target="_blank" class="image-featured"></a>
+                        <a href="https://tannerlsmith.github.io/spanish-quiz/" target="_blank"><img class="image-featured" src="../images/spanish-quiz.png"></img></a>
                         <header>
-                            <h3 class="box-h3">Project Title</h3>
+                            <h3 class="box-h3">Spanish Quiz</h3>
                         </header>
+                        {/* <img src="../images/spanish-quiz.png"></img> */}
                         <p class="postp">this is a description of the project</p>
                         <footer class="project-footer">
                             <ul class="actions">
-                                <li><a href="" target="_blank" class="button alt">Github</a></li>
+                                <li><a href="https://github.com/tannerlsmith/spanish-quiz" target="_blank" class="button alt">Github</a></li>
                             </ul>
                         </footer>
                     </section>
@@ -24,14 +25,14 @@ function Projects() {
 
                 <Col>
                     <section class="box">
-                        <a href="" target="_blank" class="image-featured"></a>
+                    <a href="https://fast-coast-75222.herokuapp.com/" target="_blank"><img class="image-featured" src="../images/budget-tracker.png"></img></a>
                         <header>
-                            <h3>Project Title</h3>
+                            <h3 class="box-h3">Budget Tracker</h3>
                         </header>
                         <p>this is a description of the project</p>
                         <footer>
                             <ul class="actions">
-                                <li><a href="" target="_blank" class="button alt">Github</a></li>
+                                <li><a href="https://github.com/tannerlsmith/budget-tracker" target="_blank" class="button alt">Github</a></li>
                             </ul>
                         </footer>
                     </section>
@@ -39,14 +40,14 @@ function Projects() {
 
                 <Col>
                     <section class="box">
-                        <a href="" target="_blank" class="image-featured"></a>
+                    <a href="https://tannerlsmith.github.io/shopping-cart/" target="_blank"><img class="image-featured" src="../images/shopping-cart.png"></img></a>
                         <header>
-                            <h3>Project Title</h3>
+                            <h3 class="box-h3">Shopping Cart</h3>
                         </header>
                         <p>this is a description of the project</p>
                         <footer>
                             <ul class="actions">
-                                <li><a href="" target="_blank" class="button alt">Github</a></li>
+                                <li><a href="https://github.com/tannerlsmith/shopping-cart" target="_blank" class="button alt">Github</a></li>
                             </ul>
                         </footer>
                     </section>
